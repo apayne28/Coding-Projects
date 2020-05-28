8c6a641bfa8d8f99941880fb2b667f1d4e8b042d
apayne28 <ashley.payne@braingu.com>
2020-05-28 11:41:16 -0400

    Worked on Like Button rendering

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -15,7 +16,8 @@ def tweet_list_view(request, *args, **kwargs):
   """
   #pylint: disable=maybe-no-member
   qs = Tweet.objects.all()
-  tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+  tweets_list = [{"id": x.id, "content": x.content,
+  "likes": random.randint(0, 123)} for x in qs]
   data = {
     "isUser": False,
     "response": tweets_list
