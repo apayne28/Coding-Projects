07287178216eb494a84ffcb44579716c1dcc6558
apayne28 <ashley.payne@braingu.com>
2020-07-07 10:49:24 -0400

    Added styling, finished app

--- pokemon/src/containers/PokemonsContainer.js
+++ pokemon/src/containers/PokemonsContainer.js
@@ -9,7 +9,7 @@ export function PokemonsContainer() {
 	 });
 
 	 return (
-		 <div className="pokemons">
+		 <div className="container">
 			 {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id}
 			 pokemon={pokemon} />)
 			 }
