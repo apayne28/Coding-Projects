f32155d9716ae5c1d39e9abc3865fe429858f675
apayne28 <ashley.payne@braingu.com>
2020-07-13 14:05:12 -0400

    Installed Mongoose and Connected App to MongoDB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//connect to mongodb database
+mongoose
+  .connect(
+    "mongodb+srv://apayne28:Thiefshipping1@gqlnin.jkxhy.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.log("Error: ", err.message));
 
 app.use(
   "/graphql",
