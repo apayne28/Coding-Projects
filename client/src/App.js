dc9152aee4e1081bd463bfd1913f8c92d3f987ab
apayne28 <ashley.payne@braingu.com>
2020-07-13 15:38:33 -0400

    Created React Front-End and Components

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//components
+import BookList from "./components/BookLists";
+
+function App() {
+  return (
+    <div id="main">
+      <h1>Ashley's Reading List</h1>
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
