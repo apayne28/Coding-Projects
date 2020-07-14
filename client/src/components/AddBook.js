166725439078e08b608cb049c5dcf3f02d23f4d1
apayne28 <ashley.payne@braingu.com>
2020-07-14 10:50:51 -0400

    Updated Component State

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,44 +1,71 @@
 import React, { Component } from "react";
-import { graphql } from "react-apollo";
+
+import { useQuery } from "@apollo/react-hooks";
+
 import { getAuthorsQuery } from "../queries/queries";
 
-class AddBook extends Component {
-  displayAuthors() {
-    var data = this.props.data;
-    if (data.loading) {
-      return <option disabled>Loading authors</option>;
-    } else {
-      return data.authors.map((author) => {
-        return (
-          <option key={author.id} value={author.id}>
-            {author.name}
-          </option>
-        );
-      });
-    }
-  }
-  render() {
-    return (
-      <form id="add-book">
-        <div className="field">
-          <label>Book name:</label>
-          <input type="text" />
-        </div>
-        <div className="field">
-          <label>Genre:</label>
-          <input type="text" />
-        </div>
-        <div className="field">
-          <label>Author:</label>
-          <select>
-            <option>Select author</option>
-            {this.displayAuthors()}
-          </select>
-        </div>
-        <button>+</button>
-      </form>
-    );
-  }
+function AddBook() {
+  const { loading, error, data } = useQuery(getAuthorsQuery);
+
+  const displayAuthorOptions = () => {
+    if (loading) return <option>loading...</option>;
+
+    let { authors } = data;
+
+    return authors.map((author) => {
+      return (
+        <option key={author.id} value={author.id}>
+          {author.name}
+        </option>
+      );
+    });
+  };
+
+  const [book, setBook] = React.useState({ name: "", genre: "", authorId: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(book);
+  };
+
+  return (
+    <form className="add-book" onSubmit={handleSubmit}>
+      <div className="field">
+        <label>Book Name:</label>
+
+        <input
+          value={book.name}
+          onChange={(e) => setBook({ ...book, name: e.target.value })}
+          type="text"
+        />
+      </div>
+
+      <div className="field">
+        <label>Book Genre:</label>
+
+        <input
+          value={book.genre}
+          onChange={(e) => setBook({ ...book, genre: e.target.value })}
+          type="text"
+        />
+      </div>
+
+      <div className="field">
+        <label>Book Author:</label>
+
+        <select
+          value={book.authorId}
+          onChange={(e) => setBook({ ...book, authorId: e.target.value })}
+        >
+          <option>Select author</option>
+          {displayAuthorOptions()}
+        </select>
+      </div>
+
+      <button>Submit</button>
+    </form>
+  );
 }
 
-export default graphql(getAuthorsQuery)(AddBook);
+export default AddBook;
