bae01a7dfffe1366d233535259bb44fb7e2ab8ca
apayne28 <ashley.payne@braingu.com>
2020-07-16 13:52:58 -0400

    Added CSS Styling . Complete

--- react-quiz/src/components/QuestionCards.tsx
+++ react-quiz/src/components/QuestionCards.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { AnswerObject } from "../App";
 
 //Styles
-//import {Wrapper, ButtonWrapper} from  './QuestionCards.styles'
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 
 type Props = {
   question: string;
@@ -22,14 +22,18 @@ const QuestionCard: React.FC<Props> = ({
   questionNum,
   totalQuestions,
 }) => (
-  <div>
+  <Wrapper>
     <p className="number">
       Question: {questionNum}/ {totalQuestions}
     </p>
     <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
       {answers.map((answer) => (
-        <div key={answer}>
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
           <button
             disabled={userAnswer ? true : false}
             value={answer}
@@ -37,10 +41,10 @@ const QuestionCard: React.FC<Props> = ({
           >
             <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
     </div>
-  </div>
+  </Wrapper>
 );
 
 export default QuestionCard;
