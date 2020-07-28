3e54e873c2199b170629d86fe58811df437d6009
apayne28 <ashley.payne@braingu.com>
2020-07-28 09:33:50 -0400

    Added Way to Handle Ajax Requests

--- tweets/views.py
+++ tweets/views.py
@@ -22,6 +22,8 @@ def tweet_create_view(request, *args, **kwargs):
     obj = form.save(commit=False)
     # do other form related logic
     obj.save()
+    if request.is_ajax():
+      return JsonResponse({}, status=201) # 201 == created items
     if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
       return redirect(next_url)
     form = TweetForm()
