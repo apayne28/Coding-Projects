f8f317aff48650dd22327a42b5b6b1830879124f
apayne28 <ashley.payne@braingu.com>
2020-07-30 12:35:48 -0400

    Created REALLY Basic Grid and Tetrominos

---
+++ script.js
@@ -0,0 +1,14 @@
+const lTetromino = "firstShape";
+const zTetromino = "secondShape";
+const oTetromino = "thirdShape";
+const iTetromino = "fouthShape";
+const tTetromino = "fifthShape";
+
+const tetrominoes = [
+  lTetromino,
+  zTetromino,
+  oTetromino,
+  iTetromino,
+  tTetromino,
+];
+console.log(tetrominoes[0]);
